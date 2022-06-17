b051cfab62529dabcab3814efcdca1f83d734ce1
zil130 <index.che@gmail.com>
2022-06-17 03:15:28 -0300

    refactor: add "!" for output greeting

--- src/cli.js
+++ src/cli.js
@@ -2,5 +2,5 @@ import readlineSync from 'readline-sync';
 
 export const greeting = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}`);
+  console.log(`Hello, ${userName}!`);
 }
