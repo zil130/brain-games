4f8fc5da434ed036f16b9004a0259ad62c9a9a22
zil130 <55101149+zil130@users.noreply.github.com>
2022-06-22 19:42:21 -0300

    refactor: moved some functions

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,11 +1,29 @@
 import readlineSync from 'readline-sync';
 import getRandomInt from '../random-int.js';
-import getCorrectAnswer from '../calculate-expr.js';
-import { operators } from '../constants.js';
 
 export const taskOfGame = 'What is the result of the expression?';
 
+const getCorrectAnswer = (x, y, operator) => {
+  let result;
+
+  switch (operator) {
+    case '+':
+      result = x + y;
+      break;
+    case '-':
+      result = x - y;
+      break;
+    case '*':
+      result = x * y;
+      break;
+    default:
+  }
+
+  return String(result);
+};
+
 export const playGame = () => {
+  const operators = ['+', '-', '*'];
   const [x, y, operator] = [
     getRandomInt(0, 10),
     getRandomInt(0, 10),
