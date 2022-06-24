aee425037a70d388ad877bc9d495555fbdcffa74
zil130 <index.che@gmail.com>
2022-06-24 06:54:40 -0300

    refactor: moved the code into a function for readability

--- src/games/even.js
+++ src/games/even.js
@@ -2,11 +2,13 @@ import getRandomInt from '../utils.js';
 
 export const taskOfGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const isEven = (num) => ((num % 2 === 0) ? 'yes' : 'no');
+
 export const playGame = () => {
   const num = getRandomInt(-20, 20);
 
   return [
     `Question: ${num}\nYour answer: `,
-    (num % 2 === 0) ? 'yes' : 'no',
+    isEven(num),
   ];
 };
