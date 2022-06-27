fcecb7eda8ad820433c48ce9fd8472d073935bf1
zil130 <55101149+zil130@users.noreply.github.com>
2022-06-27 00:05:53 -0300

    refactor: brain-progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,14 +1,15 @@
 import getRandomInt from '../utils.js';
 
 export const taskOfGame = 'What number is missing in the progression?';
+const minNum = -20;
+const maxNum = 20;
+const minStep = 2;
+const maxStep = 10;
+const minLen = 5;
+const maxLen = 10;
 
-const getProgression = () => {
+const getProgression = (firstNum, step, len) => {
   const progression = [];
-  const [firstNum, step, len] = [
-    getRandomInt(1, 20),
-    getRandomInt(2, 10),
-    getRandomInt(5, 10),
-  ];
 
   for (let i = firstNum; progression.length < len; i += step) {
     progression.push(i);
@@ -17,12 +18,20 @@ const getProgression = () => {
   return progression;
 };
 
+const getQuestionProgression = (progression, index) => {
+  const result = [...progression];
+  result[index] = '..';
+
+  return result.join(' ');
+};
+
 export const playGame = () => {
-  const progression = getProgression();
-  let question = [...progression];
+  const firstNum = getRandomInt(minNum, maxNum);
+  const step = getRandomInt(minStep, maxStep);
+  const len = getRandomInt(minLen, maxLen);
+  const progression = getProgression(firstNum, step, len);
   const searchElement = getRandomInt(0, progression.length - 1);
-  question[searchElement] = '..';
-  question = question.join(' ');
+  const question = getQuestionProgression(progression, searchElement);
   const answer = String(progression[searchElement]);
 
   return [question, answer];
