a4f31b26b8ba565c4a413afc3a893eb933a37f5f
zil130 <index.che@gmail.com>
2022-06-27 18:40:14 -0300

    refactor: brain-gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,26 +3,25 @@ import { getRandomInt } from '../utils.js';
 export const taskOfGame = 'Find the greatest common divisor of given numbers.';
 
 const getUniqueValue = (num) => {
-  const result = getRandomInt(3, 100);
+  const result = getRandomInt(2, 50);
 
   return (result !== num) ? result : getUniqueValue(num);
 };
 
-const getCorrectAnswer = (x, y) => {
-  let result;
+const isDivisor = (num, divisor) => num % divisor === 0; 
 
-  for (let i = Math.max(x, y); i > 0; i -= 1) {
-    if (x % i === 0 && y % i === 0) {
-      result = i;
-      break;
+const getCorrectAnswer = (x, y) => {
+  const min = Math.min(x, y);
+
+  for (let i = min; i > 0; i -= 1) {
+    if (isDivisor(x, i) && isDivisor(y, i)) {
+      return String(i);
     }
   }
-
-  return String(result);
 };
 
 export const playGame = () => {
-  const x = getRandomInt(2, 100);
+  const x = getRandomInt(2, 50);
   const y = getUniqueValue(x);
   const question = `${x} ${y}`;
   const answer = getCorrectAnswer(x, y);
