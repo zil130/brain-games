aaf42663c654d39fda29cd4ae9a4ae6fe537339d
zil130 <55101149+zil130@users.noreply.github.com>
2022-06-27 21:07:29 -0300

    refactor: brain-prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,21 +3,25 @@ import { getRandomInt } from '../utils.js';
 export const taskOfGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
+  if (num <= 1) {
+    return false;
+  }
+
   const sqrtNum = Math.sqrt(num);
 
   for (let i = 2; i <= sqrtNum; i += 1) {
     if (num % i === 0) {
-      return 'no';
+      return false;
     }
   }
 
-  return 'yes';
+  return true;
 };
 
 export const playGame = () => {
-  const num = getRandomInt(2, 100);
+  const num = getRandomInt(-20, 100);
   const question = num;
-  const answer = isPrime(num);
+  const answer = isPrime(num) ? 'yes' : 'no';
 
   return [question, answer];
 };
